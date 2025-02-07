17565928772a67031573e3357f665c30d9e07b6b
MarcioJRDV <mdovale21@gmail.com>
2025-02-07 23:37:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert("Your demo request has been submitted!");
+    // Here, you can integrate with an API to handle form submission, save the data, or redirect the user.
+});
